694ffdcaa4433a3789f779942485dab07e8b0494
unknown <anna-maija.rauramaa@aalto.fi>
2020-12-02 16:28:42 +0200

    set up environment

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(middleware.serveStaticFilesMiddleware);
 
 app.use(router.routes());
 
-if (!Deno.env.get('TEST_ENVIRONMENT')) {
+// if (!Deno.env.get('TEST_ENVIRONMENT')) {
   app.listen({ port: 7777 });
-}
+// }
       
 export default app;
